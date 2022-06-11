cc03c8e9888a4804f2466cd09d358b8451d42b66
sunil <sunilyadavwork2003@gmail.com>
2022-06-11 11:28:35 +0530

    UI and Responsiveness improved of Profile page, forget password and footer , set new passowrd

--- Blog/settings.py
+++ Blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-deeypapul2n#h@r+kvp7h#_&10154cu-fhbi)kkpse&2qmr*p1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sunil-code-blog.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['sunil-code-blog.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
