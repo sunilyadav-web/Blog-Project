36c987acb4c1434a51ed8fdd3cbe01e070af3f0d
sunil <sunilyadavwork2003@gmail.com>
2022-06-11 08:03:04 +0530

    change static dictory name

--- Blog/settings.py
+++ Blog/settings.py
@@ -150,7 +150,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
