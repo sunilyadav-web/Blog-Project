f2c532a17b426336fe0fef601675fa861c10b535
sunil <sunilyadavwork2003@gmail.com>
2022-06-11 10:07:25 +0530

    Changed Email of host and its App Password

--- Blog/settings.py
+++ Blog/settings.py
@@ -175,6 +175,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='sunilcodetesting@gmail.com'
-EMAIL_HOST_PASSWORD='sunilcode'
+EMAIL_HOST_USER='testdjangocode@gmail.com'
+EMAIL_HOST_PASSWORD='aucstwbuqfwrzqmc'
 
